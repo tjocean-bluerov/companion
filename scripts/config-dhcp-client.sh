3e7774021761a1ebb278b79e171da66c3afaec84
Jacob Walser <jwalser90@gmail.com>
2018-12-04 15:41:58 -0500

    network: move isc-dhcp-server hooks to interfaces up/down

--- scripts/config-dhcp-client.sh
+++ scripts/config-dhcp-client.sh
@@ -1,22 +1,5 @@
 #!/usr/bin/env bash
 
-#copy the configuration information to network-conf.txt
+#copy the configuration information to network.conf
 echo "config-client" > /home/pi/network.conf
-
-#Bring down interface eth0
-sudo ifdown eth0
-echo "Interface eth0 is down"
-
-#Bring up eth0 with DHCP Client configuration
-sudo ifup eth0
-echo "Interface eth0 is up with DHCP Client configuration"
-echo "Configuration settings for dhcp-client mode applied to Companion"
-
-#Disable dhcp server from running at boot
-sudo update-rc.d -f isc-dhcp-server remove
-
-#Stop dhcp server
-sudo service isc-dhcp-server stop
-echo "DHCP server disabled from running at boot"
-
 sudo reboot now
--- scripts/config-dhcp-server.sh
+++ scripts/config-dhcp-server.sh
@@ -1,22 +1,5 @@
 #!/usr/bin/env bash
 
-#copy the configuration information to network-conf.txt
+#copy the configuration information to network.conf
 echo "config-server" > /home/pi/network.conf
-
-#Bring down interface eth0
-sudo ifdown eth0
-echo "Interface eth0 is down"
-
-#Bring up eth0 with DHCP Server configuration
-sudo ifup eth0
-echo "Interface eth0 is up with DHCP Server configuration"
-echo "Configuration settings for dhcp-server mode applied to Companion"
-
-#Restart dhcp server 
-sudo service isc-dhcp-server restart
-
-#Enable it to run at boot
-sudo update-rc.d isc-dhcp-server defaults
-echo "DHCP server enabled to run at boot"
-
 sudo reboot now
