2bf8319cfd347d7565ea9cacc91b8acbd457df7c
Willian Galvani <williangalvani@gmail.com>
2019-03-20 21:08:16 +0000

    telem.py: use /dev/autopilot to acess pixhawk

--- tools/telem.py
+++ tools/telem.py
@@ -5,7 +5,7 @@ from time import sleep
 home = os.environ['HOME'] 
 
 while True:
-  while ((os.system("ls /dev/serial/by-id/usb-3D_Robotics_PX4_FMU_v2.x_0-if00 2>/dev/null") != 0) or (os.path.isfile(home+"/companion/scripts/start_mavproxy_telem_splitter.sh")==0)):
+  while ((os.system("ls /dev/autopilot 2>/dev/null") != 0) or (os.path.isfile(home+"/companion/scripts/start_mavproxy_telem_splitter.sh")==0)):
     sleep(2)
 
   os.system(home+"/companion/scripts/start_mavproxy_telem_splitter.sh")
